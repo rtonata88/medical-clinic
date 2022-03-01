829f1fcb63de5904ce3c732dc76595397b44ad9b
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-03-01 15:18:23 -0600

    Add invoice_items, treatments & medical_treatments

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -29,3 +29,29 @@ CREATE TABLE IF NOT EXISTS invoices(
     PRIMARY KEY(id),
 	CONSTRAINT medical_histories_fk FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
+
+CREATE TABLE IF NOT EXISTS treatments(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    type VARCHAR(100),
+    name VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS invoice_items(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT,
+    treatment_id INT,
+    PRIMARY KEY(id),
+    CONSTRAINT invoice_id_fk FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+    CONSTRAINT treatment_id_fk FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
+
+CREATE TABLE IF NOT EXISTS medical_treatments(
+    medical_history_id INT,
+    treatment_id INT,
+    CONSTRAINT medical_history_id_fk FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+    CONSTRAINT treatment_id_fk FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
